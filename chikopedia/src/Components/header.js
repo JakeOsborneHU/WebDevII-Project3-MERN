f56e7ea14509e9ecf2bff6a1f2e871d87aace9f3
FlashFrost <54652928+FlashFrost@users.noreply.github.com>
2021-05-05 17:32:59 -0500

    Modify App.js
    Move the functions to individual files.

---
+++ chikopedia/src/Components/header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Navbar, Nav} from 'react-bootstrap';
+import {LinkContainer} from 'react-router-bootstrap';
+
+function MyNavbar() {
+    return (
+        <Navbar bg="dark" variant="dark" expand="lg">
+            <Navbar.Brand to="#home">Chicopedia</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link to="#create">Create</Nav.Link>
+                    <Nav.Link to="#browse">Browse</Nav.Link>
+                    <Nav.Link to="#compare">Compare</Nav.Link>
+                    <Nav.Link to="#logout">Logout</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+}
